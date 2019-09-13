06e9e52e3eedc128ead1e762a2aaaf0c42216b41
hopes91 <nadya.me91@gmail.com>
2019-09-13 15:54:19 +0300

    add simple navbars to the header and footer + slower scrolling script

---
+++ dev/js/scrollSlower.js
@@ -0,0 +1,34 @@
+const anchorElements = document.querySelectorAll('[href^="#"]');
+
+function scrollSlower(event) {
+  event.preventDefault()
+
+  const speed = 0.3
+  const offsetY = window.pageYOffset
+  const hash = this.href.replace(/[^#]*(.*)/, '$1');
+  const topIndent = document.querySelector(hash).getBoundingClientRect().top
+  let start = null
+
+  requestAnimationFrame(step)
+
+  function step(time) {
+    if (start === null) {
+      start = time
+    }
+
+    const progress = time - start
+    let scroll = null
+
+    topIndent < 0 ?
+      scroll = Math.max(offsetY - progress/speed, offsetY + topIndent) :
+      scroll = Math.min(offsetY + progress/speed, offsetY + topIndent)
+
+    window.scrollTo(0, scroll)
+
+    scroll !== offsetY + topIndent ?
+      requestAnimationFrame(step) :
+      location.hash = hash
+  }
+}
+
+anchorElements.forEach(anchorElement => anchorElement.addEventListener('click', scrollSlower));
