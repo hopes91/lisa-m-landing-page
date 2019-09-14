dce32967c2a90f60c462c5a7ceede2b7dab5421f
hopes91 <nadya.me91@gmail.com>
2019-09-14 09:33:03 +0300

    add ;

--- dev/js/scrollSlower.js
+++ dev/js/scrollSlower.js
@@ -1,33 +1,33 @@
 const anchorElements = document.querySelectorAll('[href^="#"]');
 
 function scrollSlower(event) {
-  event.preventDefault()
+  event.preventDefault();
 
-  const speed = 0.3
-  const offsetY = window.pageYOffset
+  const speed = 0.3;
+  const offsetY = window.pageYOffset;
   const hash = this.href.replace(/[^#]*(.*)/, '$1');
-  const topIndent = document.querySelector(hash).getBoundingClientRect().top
-  let start = null
+  const topIndent = document.querySelector(hash).getBoundingClientRect().top;
+  let start = null;
 
-  requestAnimationFrame(step)
+  requestAnimationFrame(step);
 
   function step(time) {
     if (start === null) {
-      start = time
+      start = time;
     }
 
-    const progress = time - start
-    let scroll = null
+    const progress = time - start;
+    let scroll = null;
 
     topIndent < 0 ?
       scroll = Math.max(offsetY - progress/speed, offsetY + topIndent) :
-      scroll = Math.min(offsetY + progress/speed, offsetY + topIndent)
+      scroll = Math.min(offsetY + progress/speed, offsetY + topIndent);
 
-    window.scrollTo(0, scroll)
+    window.scrollTo(0, scroll);
 
     scroll !== offsetY + topIndent ?
       requestAnimationFrame(step) :
-      location.hash = hash
+      location.hash = hash;
   }
 }
 
