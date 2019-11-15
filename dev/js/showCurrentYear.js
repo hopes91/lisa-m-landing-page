9b7d55aa2d045fd0ce02ae6cc5afd2b15f393aa1
hopes91 <nadya.me91@gmail.com>
2019-11-15 12:31:03 +0300

    push to github pages

--- dev/js/showCurrentYear.js
+++
@@ -1,7 +0,0 @@
-const showCurrentYear = () => {
-  const currentYear = document.querySelector('.current-year');
-
-  currentYear.textContent = new Date().getFullYear();
-}
-
-window.addEventListener('load', showCurrentYear)
